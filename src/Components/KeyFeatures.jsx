0c2095cdbb9a4e8e2a526b972fd8fe2c1f8cee94
Yasir Khan <yk5645123@gmail.com>
2024-10-30 21:26:36 +0500

    key features section responsived

--- src/Components/KeyFeatures.jsx
+++ src/Components/KeyFeatures.jsx
@@ -27,18 +27,18 @@ export default function KeyFeatures() {
   return (
     <>
       <h2 className='text-5xl font-bold text-center' data-aos = "fade-up" data-aos-duration = "1000">Key <span className='text-blue-500'>Features</span> of Festar</h2>
-      <p data-aos = "fade-up" data-aos-duration = "1000" className='text-center mb-12 '>	
-Discover the unique features that make Festar the ideal choice for your meeting planning needs. <br /> Our platform offers seamless integration, user-friendly interfaces, and robust security to ensure your meetings are efficient and secure.
+      <p data-aos = "fade-up" data-aos-duration = "1000" className='text-center mb-12 p-4 md:pl-32 md:pr-32 '>	
+Discover the unique features that make Festar the ideal choice for your meeting planning needs. Our platform offers seamless integration, user-friendly interfaces, and robust security to ensure your meetings are efficient and secure.
 </p>
-      <div className='flex justify-around mb-16'>
+      <div className='flex flex-col justify-around mb-16 md:flex-row'>
       {data.map((item, index) => (
-            <div key={index} className="flex flex-col gap-3 w-[30%] m-1 div-shadow rounded-3xl p-2 cursor-pointer">
+            <div key={index} className="flex flex-col gap-3 w-[100%] md:w-[30%] m-1 div-shadow rounded-3xl p-2 cursor-pointer">
                 <img  className="h-2/3 w-full rounded-lg" src={item.image} alt="" />
               <h2  className="text-2xl font-bold">{item.title}</h2>
 
               <p className="text-sm">{item.description}</p>
               {/* <img className="max-h-2/4" src={item.image} alt="" /> */}
-        <button className='w-2/5 bg-white hover:bg-blue-500 font-semibold transition duration-300  pl-6 pr-6 pt-2 pb-2 border-2 border-black border-opacity-10 border-t-0 rounded-md h-12  text-blue-400 hover:text-white leading-normal font-monts shadow-lg mt-4 '>Learn More</button>
+        <button className='w-2/4 md:w-[65%] lg:w-2/5 self-center  bg-white hover:bg-blue-500 font-semibold transition duration-300  pl-6 pr-6 pt-2 pb-2 border-2 border-black border-opacity-10 border-t-0 rounded-md h-12  text-blue-400 hover:text-white leading-normal font-monts shadow-lg mt-4 '>Learn More</button>
               
             </div>
           ))}
